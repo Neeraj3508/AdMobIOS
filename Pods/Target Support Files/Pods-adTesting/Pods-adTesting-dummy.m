439f177ce46d6a361b9d123a9a8fd73d52100cad
Neeraj3508 <gneeraj32595@gmail.com>
2021-03-13 12:54:49 +0530

    Initial Commit

---
+++ Pods/Target Support Files/Pods-adTesting/Pods-adTesting-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_adTesting : NSObject
+@end
+@implementation PodsDummy_Pods_adTesting
+@end
--- adTesting/ViewController.swift
+++ adTesting/ViewController.swift
@@ -1,19 +1,33 @@
-//
-//  ViewController.swift
-//  adTesting
-//
-//  Created by Neeraj Gupta on 19/01/21.
-//
-
+import GoogleMobileAds
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var bannerView: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bannerView = GADBannerView(adSize: kGADAdSizeBanner)
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        // In this case, we instantiate the banner with desired ad size.
+        
+        
+        addBannerViewToView(bannerView)
     }
-
-
+    
+    func addBannerViewToView(_ bannerView: GADBannerView) {
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        var constraints : [NSLayoutConstraint] = [NSLayoutConstraint]()
+        constraints.append(bannerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0))
+        constraints.append(bannerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0))
+        constraints.append(bannerView.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+        constraints.append(bannerView.centerYAnchor.constraint(equalTo: view.centerYAnchor))
+        constraints.append(bannerView.heightAnchor.constraint(equalToConstant: 300))
+        NSLayoutConstraint.activate(constraints)
+        
+    }
+    
 }
-
